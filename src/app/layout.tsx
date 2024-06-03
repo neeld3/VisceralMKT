fd4cd2e009e34aa26314d7bdc058a2396ba2b639
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-06-03 16:29:30 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ViceralMR",
+  title: "VisceralMR",
   description: "Market Research Made Easy",
 };
 
