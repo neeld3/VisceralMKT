ea9318013e958967fe95d5e86916acb0b8a644be
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-06-03 13:28:06 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className="">
         <div className="max-w-4xl mx-auto p-12">
-          <div className="w-full lg:w-3/4 mx-auto mb-8">
+          <div className="w-full lg:w-4/5 mx-auto mb-5">
             <Image
               src="/vmr-logo.png" 
               alt="Logo"
