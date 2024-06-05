e164b97d6dc8704af32bce8f817b62667b8a7c70
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-06-05 15:01:51 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Marketing Research Made Easy</title>,
+        <title>Marketing Research Made Easy</title>
         <meta name="description" content="A more streamlined way to meet your surveying needs." />
       </Head>
 
@@ -123,8 +123,8 @@ export default function Home() {
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
+              }}
+            />
           </div>
         </div>
       </main>
