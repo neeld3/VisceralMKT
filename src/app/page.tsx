c201bb354c5c8919e41e5c61a7891713d06c00ef
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-06-05 15:41:58 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { DocumentTextIcon, ChartBarIcon, BanknotesIcon, CursorArrowRaysIcon } from '@heroicons/react/20/solid';
+import { DocumentTextIcon, ChartBarIcon, BanknotesIcon, CursorArrowRaysIcon, CheckCircleIcon, XCircleIcon } from '@heroicons/react/20/solid';
 
 const features = [
   {
@@ -76,7 +76,151 @@ export default function Home() {
           </div> 
         </div>
 
-
+        <div className="bg-white px-6 py-32 lg:px-8">
+          <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+            <p className="text-base font-semibold leading-7 text-indigo-600">Visceral Market Research</p>
+            <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Your AI Insights Powerhouse</h1>
+            <p className="mt-6 text-xl leading-8">
+              Times are changing. No longer is it efficient or productive to spend hours doing complicated manual work. We all know market research is a key business practice for 
+              identifying consumers and addressing their demands. What some don&apos;t know is that there&apos;s a better way to get through the time-consuming and difficult process
+              that exists today. Visceral Market Research is that better way—a comprehensive, AI-driven platform that revolutionizes how you conduct research.
+            </p>
+            <div className="mt-10 max-w-2xl">
+              <p>
+                <strong>Current Issues:</strong>
+              </p>
+              <ul role="list" className="mt-8 max-w-xl space-y-8 text-gray-600">
+                <li className="flex gap-x-3">
+                  <XCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Fragmented Tools & Process.</strong> 
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <XCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Lengthy Turnaround Times</strong> 
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <XCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Complex Learning Curves</strong> 
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <XCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Time Consuming Vendor Management.</strong> 
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <XCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Cumbersome Data Quality & Integration.</strong> 
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <XCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Slow Actionable Insighhts</strong> 
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <XCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Scalability Issues.</strong> 
+                  </span>
+                </li>
+              </ul>
+              <p className="mt-8">
+                Visceral AI is your intelligent partner, transforming the way you conduct market research.
+              </p>
+              
+              
+              <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">Visceral Market Researc&apos;s Built-In Tools:</h2>
+              <ul role="list" className="mt-8 max-w-xl space-y-8 text-gray-600">
+                <li className="flex gap-x-3">
+                  <CheckCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">End-To-End Solution.</strong> From identifying business needs 
+                    to survey development and data visualization, VisceralMR seamlessly manages every step of the research process.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckCircleIcon className="mt-1 h-5 w-5 flex-none text-cusotm-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Collaborative Workspace. </strong> Facilitates team collaboration 
+                    on methodologies and survey development, media monitoring, industry/category updates, and report development.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Qualitative Research. </strong> Utilizes AI to develop discussion guides, 
+                    review and summarize all qualitative discussions (IDIs or groups).
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Survey Programming & Hosting. </strong> Capable of programming complex quantitative 
+                    surveys and integrates panels with real-time data quality checks.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Efficient Panel Integration. </strong> Connect with panel sources more effectively, 
+                    ensuring demographic balancing and quota management. AI monitors and updates panels in real time.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Data Processing. </strong> Generate comprehensive data tables with standard outputs, 
+                    including stat testing, and more. Quickly synthesize and analyze large quantities of verbatims to identify themes and insights.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Advanced Analytics. </strong> Unlock deeper insights with sophisticated 
+                    techniques like MaxDiff, factor analysis, correlations, conjoint analysis, and predictive modeling.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Data Visualization. </strong> Transform complex data sets into clear, 
+                    actionable insights with intuitive visualization tools.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Custom Reporting. </strong> Generate customized reports that highlight key 
+                    insights and trends, tailored to your specific needs.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <CheckCircleIcon className="mt-1 h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Scalability & Flexibility. </strong> Easily scale your research efforts and 
+                    adapt to changing needs without compromising on quality or speed.
+                </li>
+              </ul>
+            </div>
+           
+            <div className="mt-16 max-w-2xl">
+              <h2 className="text-2xl font-bold tracking-tight text-gray-900">Get Ahead of the Competition</h2>
+              <p className="mt-6">
+                With one subscription you get access to ALL the tools you need. With an easy to use interface, you can rely on VisceralMR for fast, 
+                efficient, and impactful market research. Step into the future of market research with VisceralMR—where every challenge meets its match.
+              </p>
+            </div>
+          </div>
+        </div>
         
         <div className="bg-white py-16 sm:py-24">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
