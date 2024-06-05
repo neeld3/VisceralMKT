2e1823fcd7f3d01bdec2cacd2d2ebdfa4a4bb22c
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-06-05 14:48:22 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,6 +74,35 @@ export default function Home() {
             </div>
           </div> 
         </div>
+
+
+        
+        <div className="bg-white py-16 sm:py-24">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl lg:text-center">
+              <h1 className="font-permanent-marker text-custom-color text-3xl font-semibold text-center leading-7 pt-12">And That&apos;s Not All</h1>
+              <p className="mt-2 text-4xl font-bold tracking-tight sm:text-4xl">
+                An end-to-end suite that saves you time and makes you money.
+              </p>
+            </div>
+            <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+              <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+                {features.map((feature) => (
+                  <div key={feature.name} className="flex flex-col">
+                    <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                      <feature.icon className="h-5 w-5 flex-none text-custom-color" aria-hidden="true" />
+                      {feature.name}
+                    </dt>
+                    <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                      <p className="flex-auto">{feature.description}</p>
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+        </div>
+
       </main>
     </>
   );
