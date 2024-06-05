ac62d6c1842a0246f5a3d8a0a5be00ef4ed4b680
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-06-05 13:57:12 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,6 +58,8 @@ export default function Home() {
               80% Faster. 80% Cheaper. Demonstrably Better.
             </p>
           </div>
+
+          
           <div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
             <div className="absolute inset-0 -z-10">
               <svg
@@ -73,7 +75,7 @@ export default function Home() {
                 <rect width="100%" height="100%" strokeWidth={0} fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
               </svg>
             </div>
-            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+            <div className="mx-auto grid max-w-4xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
               <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
                 <div className="lg:pr-4">
                   <div className="lg:max-w-lg">
